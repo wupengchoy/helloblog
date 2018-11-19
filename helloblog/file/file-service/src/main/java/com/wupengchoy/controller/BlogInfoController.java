0e4a292564aabefa46475627e995990819aec314
wupengchoy <15962780713@163.com>
2018-11-19 18:03:00 +0800

    add

--- helloblog/file/file-service/src/main/java/com/wupengchoy/controller/BlogInfoController.java
+++ helloblog/file/file-service/src/main/java/com/wupengchoy/controller/BlogInfoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by Jeremy on 11/16/18.
+ * Created by Jeremy on 11/16/18.--test git
  */
 @RestController
 public class BlogInfoController {
