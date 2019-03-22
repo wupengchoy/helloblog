d577ff65cf82223ee8c8516975f9c4bec6452b20
wupengchoy <15962780713@163.com>
2019-03-23 00:11:12 +0800

    保存《算法》第四版 涉及的algs4.jar库

--- parent/mystudy/src/main/java/com/wupengchoy/mystudy/JustTest.java
+++ parent/mystudy/src/main/java/com/wupengchoy/mystudy/JustTest.java
@@ -1,5 +1,7 @@
 package com.wupengchoy.mystudy;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,15 +10,16 @@ import java.util.stream.Collectors;
 public class JustTest {
     public static void main(String[] args)
     {
-        Map<String,String> map = new HashMap<>();
-        map.put("1",null);
-        map.put("2","2");
-        map.put("3",null);
-        map.put("4","4");
-
-       List<String> list = map.values().stream().filter(str->str!=null).collect(Collectors.toList());
-
-       list.forEach(str-> System.out.println(str));
+//        Map<String,String> map = new HashMap<>();
+//        map.put("1",null);
+//        map.put("2","2");
+//        map.put("3",null);
+//        map.put("4","4");
+//
+//       List<String> list = map.values().stream().filter(str->str!=null).collect(Collectors.toList());
+//
+//       list.forEach(str-> System.out.println(str));
+        System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy年MM月")));
 
     }
 }
